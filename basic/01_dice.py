30080c1d45651f83160534171c6f345d8ddb83c3
bullhorn7 <bullhorn7@gmail.com>
2015-09-25 11:35:52 +0300

    Removes spaces for consistency

--- basic/01_dice.py
+++ basic/01_dice.py
@@ -9,11 +9,9 @@ while True:
 
     if roll in ("quit", "exit"):
         break
-
     elif roll in ("d4", "d6", "d8", "d10", "d12", "d20", "d100"):
         rolled = int(roll[1:])
         rolled = random.randint(1, rolled)
         print("You rolled a " + roll + " and it landed on: " + str(rolled))
-
     else:
         print("This is not a real die! try again with d4, d6, d8, d10, d12, d20 or d100")
