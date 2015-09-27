3eb0d8518d561da26e4fbabe1bc92eab27a2ef0a
bullhorn7 <bullhorn7@gmail.com>
2015-09-27 23:00:02 +0300

    Tiny function definition program

---
+++ basic/04_functiondef/04_functiondef.py
@@ -0,0 +1,8 @@
+# Let's define a function that takes a word as an argument
+# and checks if it starts with a vowel
+
+names = ["Archibald", "Beelzebob", "Chupakabra", "Drizzt", "Elreon"]
+
+def is_start_with_vowel(word):
+    word[0] in "AEOUI"
+    return word
