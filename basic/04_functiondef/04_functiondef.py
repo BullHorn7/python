5c6b1b85e626ebde0f5ce4c1ae28aeef4775aff3
bullhorn7 <bullhorn7@gmail.com>
2015-09-27 23:03:43 +0300

    I don't even

--- basic/04_functiondef/04_functiondef.py
+++ basic/04_functiondef/04_functiondef.py
@@ -1,8 +1,7 @@
 # Let's define a function that takes a word as an argument
 # and checks if it starts with a vowel
 
-names = ["Archibald", "Beelzebob", "Chupakabra", "Drizzt", "Elreon"]
-
 def is_start_with_vowel(word):
-    word[0] in "AEOUI"
-    return word
+    return word[0] in "AEOUI"
+
+names = ["Archibald", "Beelzebob", "Chupakabra", "Drizzt", "Elreon"]
