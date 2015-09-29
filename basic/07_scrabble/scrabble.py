4ec2adc16f1475361fd0fcbbd6f442f49cfd5802
BullHorn7 <bullhorn7@gmail.com>
2015-09-30 00:54:25 +0200

    Complete Scrabble cheater program

--- basic/07_scrabble/scrabble.py
+++ basic/07_scrabble/scrabble.py
@@ -1,19 +1,35 @@
-import sys
+scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
 
-scores = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2,
-         "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3,
-         "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1,
-         "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4,
-         "X": 8, "Z": 10}
-
-dictionary = []
+word_list = []
 filename = open("sowpods.txt", "r")
 for word in filename:
-    cleanword = word.strip()
-    lowercleanword = str.lower(cleanword)
-    dictionary.append(lowercleanword)
+    cleanword = word.strip().lower()
+    word_list.append(cleanword)
 filename.close()
 
-print("Welcome to the Scrabble cheater!")
-print("This program contains " + str(len(dictionary)) + " valid Scrabble words")
+print("\nWelcome to the Scrabble cheater!")
+print("This program contains " + str(len(word_list)) + " valid Scrabble words")
 
+valid_words = []
+rack = list(input("\nWhat letters do you have in your Scrabble rack?\n-->"))
+for word in word_list:
+    available_letters = rack[:]
+    valid = True
+    for letter in word:
+        if letter not in available_letters:
+            valid = False
+            break
+        available_letters.remove(letter)
+
+    if valid:
+        score = 0
+        for letter in word:
+            score = score + scores[letter]
+        valid_words.append((score, word))
+
+for play in sorted(valid_words):
+    print(str(play[0]) + " " + (play[1]))
